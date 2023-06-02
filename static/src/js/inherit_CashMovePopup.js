3273e69168fbd91aaa911d8f6173102fbbcafc36
Tanimou cisse <tanimou@live.fr>
2023-06-02 11:09:23 +0000

    Refactor popup and close-popup templates to fix formatting discrepancies.
    This commit refactors the popup template and close-popup template by improving the formatting to correct discrepancies. In the popup template, a div element and a footer element were added to the xpath while in the close-popup template, a style attribute was added to a div element to fix formatting differences.

--- static/src/js/inherit_CashMovePopup.js
+++ static/src/js/inherit_CashMovePopup.js
@@ -15,19 +15,11 @@ odoo.define('pos_custome.InheritCashMovePopup', function (require) {
                     inputType: '', // '' | 'in' | 'out'
                     inputAmount: '',
                     inputReason: '',
-                    inputCurrency: 'XOF',
+                    inputCurrency: this.env.pos.get_order().pos.currency.name,
                     inputHasError: false,
                 });
             }
-            // reasonValues() {
-            //     const values = [{ 'name': ' ', 'value': 1 }, { 'name': 'Avance sur salaire', 'value': 2 },
-            //     { 'name': 'Taxi', 'value': 3 }, { 'name': 'Salaire', 'value': 4 },
-            //     { 'name': 'CIE', 'value': 5 }, { 'name': 'SODECI', 'value': 6 },
-            //         { 'name': 'Matériels de nettoyage', 'value': 7 }, { 'name': 'Matériels caisse', 'value': 8 }, { 'name': 'Autres', 'value': 9 }]
-            //     // const active_currencies = inherit_models.new_currency
-            //     // console.log("active_currencies", active_currencies)
-            //                 return values
-            // }
+
              active_currency() {
                  console.log("compleme,tary_currency", this.env.pos)
                  return [{ 'name': this.env.pos.get_order().pos.currency.name },{ 'name': this.env.pos.config.complementary_currency[1] }];
